9f7c6060c2ca9cd8a028d1ec0ed12bb50df567f5
Victor Hugo <vct.bta@gmail.com>
2017-11-11 11:29:58 -0200

    Une a instalação de desinstalação em um arquivo

--- install.sh
+++
@@ -1,5 +0,0 @@
-script_file='/usr/bin/docker-gen-env'
-sudo cp docker-gen-env.sh $script_file
-sudo chmod +x $script_file
-echo 'Comando instalado com sucesso!'
-
--- installer.sh
+++ installer.sh
@@ -3,6 +3,8 @@ if [ -e $script_file ]; then
 	sudo rm $script_file
 	echo 'Comando docker-gen-env desinstalado com sucesso!'
 else
-	echo 'Comando não instalado previamente!'
+	sudo cp docker-gen-env.sh $script_file
+	sudo chmod +x $script_file
+	echo 'Comando instalado com sucesso!'
 fi
 
